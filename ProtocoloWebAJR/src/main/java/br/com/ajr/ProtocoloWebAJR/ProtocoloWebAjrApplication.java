f8a1fd8d8352fa26d1ef256ba655188b66f67c36
jorgeHAO <jorge_computacao@hotmail.com>
2017-01-06 22:09:47 -0200

    criação do projeto

---
+++ ProtocoloWebAJR/src/main/java/br/com/ajr/ProtocoloWebAJR/ProtocoloWebAjrApplication.java
@@ -0,0 +1,12 @@
+package br.com.ajr.ProtocoloWebAJR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocoloWebAjrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocoloWebAjrApplication.class, args);
+	}
+}
